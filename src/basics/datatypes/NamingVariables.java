10735a073463c2717d3b528ccdb608cb25f3b3c6
Felipeff666 <61770070+Felipeff666@users.noreply.github.com>
2025-08-11 21:46:46 -0400

    feat: variable namig convention

---
+++ src/basics/datatypes/NamingVariables.java
@@ -0,0 +1,11 @@
+package basics.datatypes;
+
+public class NamingVariables {
+    //all variables in java are case-sensitive this must begin with a letter not a number, dollar sign $, underscore _ and contains each one name lately
+    //the following variables are valid:
+    int myVariable;
+    int _private_variable;
+    int salary_increase;
+    int $discount;
+
+}
