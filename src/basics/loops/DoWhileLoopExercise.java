39c0df426144e68502be2474650cfb9071153dfa
Felipeff666 <61770070+Felipeff666@users.noreply.github.com>
2025-08-19 22:30:46 -0400

    feat: exercises with loops

---
+++ src/basics/loops/DoWhileLoopExercise.java
@@ -0,0 +1,13 @@
+package basics.loops;
+
+public class DoWhileLoopExercise {
+    public static void main(String[] args) {
+
+     int counter = 1;
+     do{
+         System.out.println(counter);
+         counter++;
+     }while(counter <= 5);
+
+    }
+}
---
+++ src/basics/loops/ForLoopExercise.java
@@ -0,0 +1,17 @@
+package basics.loops;
+
+public class ForLoopExercise {
+    public static void main(String[] args) {
+
+        /*
+            TODO
+            print numbers from 1 to 5 to the console using 'for' loop
+            use System.out.println to print values
+        */
+
+        for(int i = 1 ; i <= 5; i++){
+            System.out.println(i);
+        }
+
+    }
+}
