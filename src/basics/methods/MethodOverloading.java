738a34d0ca9a0b196bfd051a28880ede1c5df74c
Felipeff666 <61770070+Felipeff666@users.noreply.github.com>
2025-08-21 23:31:50 -0400

    method overloading

--- src/basics/methods/MethodOverloading.java
+++ src/basics/methods/MethodOverloading.java
@@ -7,6 +7,9 @@ public class MethodOverloading {
         System.out.println(add(2.1,1.5));
 
     }
+    // both of the allows methods have the same name but different parameters
+    // that is possible and help us to be more practice
+
     public static int add(int a, int b){
         return a+b;
     }
