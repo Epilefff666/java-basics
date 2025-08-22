e39f8a8cc8c2555f50be52a8e10ecae5b9af8ddf
Felipeff666 <61770070+Felipeff666@users.noreply.github.com>
2025-08-21 23:29:56 -0400

    method overloading

---
+++ src/basics/methods/MethodOverloading.java
@@ -0,0 +1,16 @@
+package basics.methods;
+
+public class MethodOverloading {
+    public static void main(String[] args) {
+
+        System.out.println(add(1,2));
+        System.out.println(add(2.1,1.5));
+
+    }
+    public static int add(int a, int b){
+        return a+b;
+    }
+    public static double add(double a, double b){
+        return a+b;
+    }
+}
